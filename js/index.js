1d2402e848a98fb53c4e204989612bf85bd0353c
Nicole Sandry <nms377@nyu.edu>
2018-05-22 15:03:17 -1000

    I was going to create a form carousel that includes a form for shipping info, payment info, and order reviews. I began building the forms and its components in JS but decided to create the elements in the index file instead. For my next revision I will try to include input masks to format input fields.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// function ccCheckout() {
+// 	// Shipping Container 
+// 	var shippingContainer = document.createElement('div');
+// 	shippingContainer.id = 'shippingContainer';
+// 	funcContainer.appendChild(shippingContainer);
+
+// 	var checkoutForm = document.createElement('form');
+// 	funcContainer.appendChild(checkoutForm);
+
+// 	var firstName = document.createElement('input');
+// 	firstName.type = 'text';
+// 	firstName.id = firstName;
+// 	checkoutForm.appendChild(firstName);
+
+// 	var firstNameLbl = document.createElement('label');
+// 	var fName = document.createTextNode('First Name');
+// 	firstNameLbl.setAttribute("for", "firstName");
+// 	firstNameLbl.appendChild(fName);
+// 	checkoutForm.appendChild(firstNameLbl);
+
+// }
+
+// ccCheckout();
